40bed64a8faed914806de365b903864ff89661d3
chacha86 <taejincha0619@gmail.com>
2026-09-02 11:43:51 +0900

    21-apikey == username

--- backend/src/main/java/com/back/p67260811/domain/member/entity/Member.java
+++ backend/src/main/java/com/back/p67260811/domain/member/entity/Member.java
@@ -24,7 +24,10 @@ public class Member extends BaseEntity {
         this.username = username;
         this.password = password;
         this.nickname = nickname;
-        this.apiKey = UUID.randomUUID().toString();
     }
 
+    public Member(String username, String password, String nickname, String apiKey) {
+        this(username, password, nickname);
+        this.apiKey = apiKey;
+    }
 }
