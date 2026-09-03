626610a36345be3ea48dabf208abba49a964869a
chacha86 <taejincha0619@gmail.com>
2026-09-03 11:27:52 +0900

    43

--- backend/src/main/java/com/back/p67260811/global/rq/Rq.java
+++ backend/src/main/java/com/back/p67260811/global/rq/Rq.java
@@ -3,7 +3,9 @@ package com.back.p67260811.global.rq;
 import com.back.p67260811.domain.member.entity.Member;
 import com.back.p67260811.domain.member.service.MemberService;
 import com.back.p67260811.global.exception.ServiceException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
@@ -14,6 +16,7 @@ public class Rq {
 
     private final MemberService memberService;
     private final HttpServletRequest request;
+    private final HttpServletResponse response;
 
     public Member getActor() {
 
@@ -31,6 +34,12 @@ public class Rq {
                 .orElseThrow(() -> new ServiceException("401-3", "API 키가 올바르지 않습니다."));
 
         return actor;
+    }
+
+    public void addCookie(String name, String value) {
+        response.addCookie(
+                new Cookie(name, value)
+        );
     }
 
 }
