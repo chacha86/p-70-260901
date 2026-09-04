86a88302c24aa70917d27f982101b7254c9c9def
chacha86 <taejincha0619@gmail.com>
2026-09-04 11:49:10 +0900

    59

--- backend/src/main/java/com/back/p67260811/domain/member/service/AuthTokenService.java
+++ backend/src/main/java/com/back/p67260811/domain/member/service/AuthTokenService.java
@@ -2,6 +2,7 @@ package com.back.p67260811.domain.member.service;
 
 import com.back.p67260811.domain.member.entity.Member;
 import com.back.p67260811.standard.Ut;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -9,8 +10,11 @@ import java.util.Map;
 @Service
 public class AuthTokenService {
 
-    private String secretPattern= "abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890";
-    private long expireMills = 1000L * 60 * 10;
+    @Value("${custom.jwt.secret-key}")
+    private String secretPattern;
+
+    @Value("${custom.jwt.expireMills}")
+    private long expireMills;
 
     public String genAccessToken(Member member) {
 
@@ -30,7 +34,6 @@ public class AuthTokenService {
 
         int id = (int)payload.get("id");
         String username = (String)payload.get("username");
-
 
         return Map.of("id", id, "username", username);
     }
--- backend/src/test/java/com/back/p67260811/domain/membmer/service/AuthTokenServiceTest.java
+++ backend/src/test/java/com/back/p67260811/domain/membmer/service/AuthTokenServiceTest.java
@@ -9,6 +9,7 @@ import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,9 +30,11 @@ public class AuthTokenServiceTest {
     @Autowired
     private MemberRepository memberRepository;
 
-    private long expireMills = 1000L * 60 * 10;
-    private String secretPattern= "abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890";
+    @Value("${custom.jwt.secret-key}")
+    private String secretPattern;
 
+    @Value("${custom.jwt.expireMills}")
+    private long expireMills;
 
     @Test
     @DisplayName("authTokenService 서비스가 존재한다.")
