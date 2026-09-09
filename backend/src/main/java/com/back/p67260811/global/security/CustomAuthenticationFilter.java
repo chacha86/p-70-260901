ffcd9854bca697607d7b20059f60f0a72b2d029a
chacha86 <taejincha0619@gmail.com>
2026-09-09 09:50:32 +0900

    84

--- backend/src/main/java/com/back/p67260811/global/security/CustomAuthenticationFilter.java
+++ backend/src/main/java/com/back/p67260811/global/security/CustomAuthenticationFilter.java
@@ -9,6 +9,11 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -87,6 +92,22 @@ public class CustomAuthenticationFilter extends OncePerRequestFilter {
             rq.addCookie("accessToken", newAccessToken);
             rq.setHeader("accessToken", newAccessToken);
         }
+
+        UserDetails user = new User(
+                member.getUsername(),
+                "",
+                List.of()
+        );
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                user,
+                user.getPassword(),
+                user.getAuthorities()
+        );
+
+        SecurityContextHolder
+                .getContext()
+                .setAuthentication(authentication);
 
         filterChain.doFilter(request, response);
     }
