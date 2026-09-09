8e0cd2830c7eb7645f8524bc1dbe6f002b98b124
chacha86 <taejincha0619@gmail.com>
2026-09-09 12:10:31 +0900

    91

--- backend/src/test/java/com/back/p67260811/domain/post/post/controller/ApiV1AdmPostControllerTest.java
+++ backend/src/test/java/com/back/p67260811/domain/post/post/controller/ApiV1AdmPostControllerTest.java
@@ -71,7 +71,7 @@ public class ApiV1AdmPostControllerTest {
         resultActions
                 .andExpect(status().isForbidden())
                 .andExpect(jsonPath("$.resultCode").value("403-1"))
-                .andExpect(jsonPath("$.msg").value("권한이 없습니다"));
+                .andExpect(jsonPath("$.msg").value("권한이 없습니다."));
 
     }
 }
